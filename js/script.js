a76bbcc2cd336423d78ccf9f802bf86d0cf01fe9
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-07-13 09:36:40 +0000

    Added coinSums function at script.js

--- js/script.js
+++ js/script.js
@@ -17,3 +17,28 @@ function getCoinInfo() {
     // Display Information in the Browser
     document.getElementById("coininfo").innerHTML = txt;
 }
+
+/*
+    Function to return the number of different ways to make n pence using
+    any number of coins.
+
+    coinSums(50)  returns 451
+    coinSums(100) returns 4563
+    coinSums(150) returns 21873
+    coinSums(200) returns 73682
+
+*/
+function coinSums(n) {
+    let coins = [1,2,5,10,20,50,100,200];
+    // Apply Dynamic Programming
+    let numWays = Array(n+1).fill(0);
+    numWays[0] = 1;
+    coins.forEach((coin) => {
+        for (let i=coin;i<=n;i++) {
+            numWays[i] += numWays[i-coin];
+        }
+    });
+    // Return the last element in the array
+    return numWays[n];
+}
+
